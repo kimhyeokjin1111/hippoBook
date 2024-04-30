97a3df1a643f318a43843122238823f458f9f15d
kimhyeokjin1111 <r3rumasi2@gmail.com>
2024-04-30 12:37:26 +0900

    관리자 페이지 컨트롤러 연결 마이페이지 컨트롤러 연결 (관리, 탈퇴, 수정, 스티커) 게시판 컨트롤러 연결(메인, 뷰)

--- src/main/java/com/example/hippobookproject/controller/book/BookController.java
+++ src/main/java/com/example/hippobookproject/controller/book/BookController.java
@@ -4,10 +4,18 @@ import com.example.hippobookproject.service.book.BookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
+@RequestMapping("/book")
 @RequiredArgsConstructor
 public class BookController {
     private final BookService bookService;
+
+    @GetMapping("/info")
+    public String bookInfo(){
+        return "book/book_info_page";
+    }
 }
--- src/main/java/com/example/hippobookproject/controller/mypage/MyPageController.java
+++ src/main/java/com/example/hippobookproject/controller/mypage/MyPageController.java
@@ -3,10 +3,31 @@ package com.example.hippobookproject.controller.mypage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
+@RequestMapping("/mypage")
 @RequiredArgsConstructor
 public class MyPageController {
+    @GetMapping("/sticker")
+    public String myPageSticker(){
+        return "mypage/myPageInt_stiker";
+    }
 
+    @GetMapping("/manage")
+    public String myPageManage(){
+        return "mypage/myPageInt_manage";
+    }
+
+    @GetMapping("/modify")
+    public String myPageModify(){
+        return "mypage/myPageInt_modify";
+    }
+
+    @GetMapping("/secession")
+    public String myPageSecession(){
+        return "mypage/myPageInt_secession";
+    }
 }
