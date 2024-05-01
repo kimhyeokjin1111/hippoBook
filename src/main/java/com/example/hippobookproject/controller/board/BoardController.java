20196ea015ff220b65dd97498ae13ce53ebcf82b
Kimsj112 <sujeong6254@gmail.com>
2024-05-01 21:48:17 +0900

    search,feed-postwrite/readwrite,writepage(게시판) -> 컨트롤러 타임리프 완료

--- src/main/java/com/example/hippobookproject/controller/board/BoardController.java
+++ src/main/java/com/example/hippobookproject/controller/board/BoardController.java
@@ -4,10 +4,18 @@ import com.example.hippobookproject.service.board.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
+@RequestMapping("/board")
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
+
+    @GetMapping("/write")
+    public String writepage() {
+        return "board/writepage";
+    }
 }
