e2c0f329607e1d9da59e7d4f6e5e20985ac17b40
Kimsj112 <sujeong6254@gmail.com>
2024-05-02 13:32:33 +0900

    feed-card 안의 데이터 뿌리기 완료 (05.02) db > feed like 열 추가, feedlike 테이블 추가, feedlike시퀀스 추가/ feedfile 테이블 추가, feedfile 시퀀스 추가 => 각 테이블 테스트 데이터 추가(fk 받은 테이블들 데이터도) feed (xml,mapper,test,service)파일 생성 및 입력 - selectAll

--- src/main/java/com/example/hippobookproject/controller/feed/FeedController.java
+++ src/main/java/com/example/hippobookproject/controller/feed/FeedController.java
@@ -1,13 +1,17 @@
 package com.example.hippobookproject.controller.feed;
 
+import com.example.hippobookproject.dto.feed.CardDto;
 import com.example.hippobookproject.service.feed.FeedService;
 import com.example.hippobookproject.service.follow.FollowService;
 import com.example.hippobookproject.service.recommend.RecommendService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -19,7 +23,9 @@ public class FeedController {
     private final FollowService followService;
 
     @GetMapping()
-    public String feedPage(){
+    public String feedPage(Model model){
+        List<CardDto> feedList = feedService.selectAll();
+        model.addAttribute("feedList", feedList);
         return "feed/feedpage";
     }
 
--- src/main/java/com/example/hippobookproject/service/feed/FeedService.java
+++ src/main/java/com/example/hippobookproject/service/feed/FeedService.java
@@ -1,13 +1,20 @@
 package com.example.hippobookproject.service.feed;
 
+import com.example.hippobookproject.dto.feed.CardDto;
 import com.example.hippobookproject.mapper.feed.FeedMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class FeedService {
     private final FeedMapper feedMapper;
+
+    public List<CardDto> selectAll(){
+        return feedMapper.selectAll();
+    }
 }
