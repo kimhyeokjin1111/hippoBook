766b8e41b33ccfe374380fd464c1bd49aaaa76a5
Kimsj112 <sujeong6254@gmail.com>
2024-05-03 13:01:17 +0900

    login, main, feed,follow페이지 링크(a)처리 xml - insert 추가 selectall 수정 follow api 파일 생성 팔로우 버튼 클릭 시 팔로우 api - follow 데이터 보내는 js 추가

---
+++ src/main/java/com/example/hippobookproject/config/WebConfig.java
@@ -0,0 +1,34 @@
+package com.example.hippobookproject.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${file.dir}")
+    private String fileDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/upload/**")
+                .addResourceLocations("file:" + fileDir);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
