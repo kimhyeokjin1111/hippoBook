482ba9fd3d1d83437209a40da3f64fd467cbdbe5
kimhyeokjin1111 <r3rumasi2@gmail.com>
2024-05-14 13:45:29 +0900

    관리자 스티커 페이지 데이터 화면에 띄우기 성공

--- src/main/resources/static/js/administrator/admin_follow.js
+++ src/main/resources/static/js/administrator/admin_follow.js
@@ -39,3 +39,82 @@
     }
   });
 }
+
+{
+  let $followCheck = document.querySelector('.main__followgoal-td');
+  let $permission = document.querySelectorAll('.permission');
+
+  $permission.forEach(p => {
+    p.addEventListener('click', function (){
+      $followCheck.dataset.check = p.value;
+    })
+  })
+}
+
+{
+  let $searchBtn = document.querySelector('.main__user-search-btn');
+  let $followUserName = document.querySelector('.main__userInfo-input');
+  let $fStartDate = document.querySelector('.startDate');
+  let $fEndDate = document.querySelector('.endDate');
+  let $followCheck = document.querySelector('.main__followgoal-td');
+  let $rownum = document.querySelector('.main__rownum-select');
+
+  $searchBtn.addEventListener('click', function (){
+    let followStikerInfo = {
+      followUserName : $followUserName.value,
+      fStartDate : $fStartDate.value,
+      fEndDate : $fEndDate.value,
+      followCheck : $followCheck.dataset.check || ""
+    }
+
+    let params = new URLSearchParams(followStikerInfo);
+    let rowValue = document.querySelector('.main__rownum-select').value;
+    console.log(rowValue)
+    console.log(params.toString())
+    fetch(`/v1/admin/follow?${params.toString()}&amount=${rowValue}`)
+        .then(list => list.json())
+        .then(l => {
+          let tags = '';
+          let resultSticker = document.querySelector('.main__result-list-container');
+
+          for (let i = 0; i < l.followList.length; i++) {
+            console.log(l.followList[i])
+            let name = l.followList[i].userName;
+            let loginId = l.followList[i].userNickname;
+            let gender = l.followList[i].userGender;
+            let age = l.followList[i].userAge;
+            let followCnt = l.followList[i].followCnt;
+            let skickerDate = l.followList[i].skickerDate
+
+            console.log(name)
+            console.log(loginId)
+            console.log(gender)
+            console.log(age)
+            console.log(followCnt)
+            console.log(skickerDate)
+
+            tags += `<ul>
+                      <li>
+                        <label>
+                          <input
+                            type="checkbox"
+                            name="checkOne"
+                            class="main__result-checkone"
+                          />
+                        </label>
+                      </li>
+                      <li>${name}</li>
+                      <li>${loginId}</li>
+                      
+                      <li>${gender}</li>
+                      <li>${age}</li>
+                      <li>${followCnt}</li>
+                      <li>${skickerDate}</li>
+                    </ul>
+                    `
+          }
+
+          resultSticker.innerHTML = tags;
+        })
+  })
+}
