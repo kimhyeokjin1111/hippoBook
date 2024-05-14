ec418e05057260212db4413c053b721d948730bc
Mrcho27 <qhdekdaos@naver.com>
2024-05-14 11:33:44 +0900

    "마이페이지 최근담은 책 완료"

--- src/main/java/com/example/hippobookproject/mapper/user/MypageMapper.java
+++ src/main/java/com/example/hippobookproject/mapper/user/MypageMapper.java
@@ -1,9 +1,12 @@
 package com.example.hippobookproject.mapper.user;
 
+import com.example.hippobookproject.dto.mypage.BookContainerDto;
 import com.example.hippobookproject.dto.mypage.IntBoardDto;
 import com.example.hippobookproject.dto.mypage.IntProfileDto;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.awt.print.Book;
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
@@ -16,4 +19,7 @@ public interface MypageMapper {
 
 
     void updateIntBoardText(IntBoardDto intBoardDto);
+
+    List<BookContainerDto> selectRecentBook(Long userId);
+
 }
