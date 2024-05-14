f49dffb772761040c051ce8a5fe48f92e8edbb6d
seoyeon0724 <seoyom0724@naver.com>
2024-05-14 11:43:04 +0900

    파일 미리 보기 뿌리기 maincontroller 수정 main css html 수정

--- src/main/java/com/example/hippobookproject/controller/alarm/alarmController.java
+++ src/main/java/com/example/hippobookproject/controller/alarm/alarmController.java
@@ -4,13 +4,20 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class alarmController {
+
     @GetMapping("/alarm")
     public String alarmPage(){
         return "/alarm/alarmpage";
     }
+//    @GetMapping("/alarm")
+//    public String alarmPage(@SessionAttribute(value = "userId", required = false )
+//                            Long userId){
+//        return "/alarm/alarmpage";
+//    }
 }
