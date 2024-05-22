cffb8751ffe9b3a29c4041113ad4870ef0fff1fa
seoyeon0724 <seoyom0724@naver.com>
2024-05-22 13:11:54 +0900

    alarm 작업 중 /  fragment/header 수정 중

--- src/main/java/com/example/hippobookproject/mapper/alarm/AlarmMapper.java
+++ src/main/java/com/example/hippobookproject/mapper/alarm/AlarmMapper.java
@@ -4,12 +4,14 @@ import com.example.hippobookproject.dto.alarm.AlarmDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-import java.util.Optional;
 
 @Mapper
     public interface AlarmMapper {
     List<AlarmDto> selectAlarm();
     List<AlarmDto> findByID(Long userId);
     void insertAlarm(AlarmDto alarmDto);
+    void updateAlarmCheckByUserId(Long userId);
+
+    Boolean hasUncheckedAlarms(Long userId);
 
     }
