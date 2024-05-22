db9bae22415d38b5e854e91c4b97da25b91f56cd
kimhyeokjin1111 <r3rumasi2@gmail.com>
2024-05-22 13:11:11 +0900

    챗봇 이전 내용 기억 기능 임시 완성

--- src/main/java/com/example/hippobookproject/api/chatbot/ChatbotApi.java
+++ src/main/java/com/example/hippobookproject/api/chatbot/ChatbotApi.java
@@ -1,5 +1,6 @@
 package com.example.hippobookproject.api.chatbot;
 
+import com.example.hippobookproject.dto.chatbot.ChatbotRespDto;
 import com.example.hippobookproject.service.chatbot.ChatbotService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,10 +8,7 @@ import okhttp3.Headers;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -26,8 +24,15 @@ public class ChatbotApi {
     private final ChatbotService chatbotService;
 
     @PostMapping("/v1/ai/reply")
-    public Map chatbotTest(@RequestBody Map<String, String> msg){
-        log.info("msg = " + msg);
-        return chatbotService.respMap(msg);
+    public ChatbotRespDto chatbotTest(@RequestBody Map<String, String> msg,
+                                      @SessionAttribute("userId") Long userId ){
+//        log.info("msg = " + msg);
+        log.info("msg = " + msg + ", userId = " + userId);
+        return chatbotService.respMap(msg, userId);
+    }
+
+    @DeleteMapping("/v1/ai")
+    public void exitChatbot(@SessionAttribute("userId") Long userId){
+        chatbotService.removeChatHistory(userId);
     }
 }
