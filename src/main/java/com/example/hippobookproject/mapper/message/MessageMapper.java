e0e3df3a49870a333ba66b9101d42fd4316d3d0c
Mrcho27 <qhdekdaos@naver.com>
2024-05-22 13:04:09 +0900

    "받은쪽지함 , 보낸 쪽지함 리스트 select 완료 " " 쪽지 쓰기 insert 해야함"

--- src/main/java/com/example/hippobookproject/mapper/message/MessageMapper.java
+++ src/main/java/com/example/hippobookproject/mapper/message/MessageMapper.java
@@ -1,8 +1,19 @@
 package com.example.hippobookproject.mapper.message;
 
+import com.example.hippobookproject.dto.message.MessageDto;
+import com.example.hippobookproject.dto.user.UserJoinDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface MessageMapper {
+    void insertMessage(MessageDto messageDto);
 
+    void selectUserNickname(String userNickname);
+
+    List<MessageDto> selectGetMessage(Long userId);
+
+    List<MessageDto> selectPostMessage(Long userId);
 }
--- src/main/java/com/example/hippobookproject/service/message/MessageService.java
+++ src/main/java/com/example/hippobookproject/service/message/MessageService.java
@@ -1,13 +1,35 @@
 package com.example.hippobookproject.service.message;
 
+import com.example.hippobookproject.dto.message.MessageDto;
+import com.example.hippobookproject.dto.mypage.BookContainerDto;
 import com.example.hippobookproject.mapper.message.MessageMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class MessageService {
     private final MessageMapper messageMapper;
+
+    public void findUserNickname(String userNickname){
+        messageMapper.selectUserNickname(userNickname);
+    };
+
+    public void registerMessageWrite(MessageDto messageDto){
+        messageMapper.insertMessage(messageDto);
+    }
+
+    public List<MessageDto> findGetMessage(Long userId) {
+
+        return messageMapper.selectGetMessage(userId);
+    }
+
+    public List<MessageDto> findPostMessage(Long userId) {
+
+        return messageMapper.selectPostMessage(userId);
+    }
 }
