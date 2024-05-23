3fabe7df49ffc9c15a89fbfd7f99790a4c1be296
seoyeon0724 <seoyom0724@naver.com>
2024-05-23 13:15:39 +0900

    알람 처리 완료 / css수정해야힘

--- src/main/java/com/example/hippobookproject/mapper/alarm/AlarmMapper.java
+++ src/main/java/com/example/hippobookproject/mapper/alarm/AlarmMapper.java
@@ -1,17 +1,23 @@
 package com.example.hippobookproject.mapper.alarm;
 
 import com.example.hippobookproject.dto.alarm.AlarmDto;
+import com.example.hippobookproject.dto.feed.FollowDto;
+import com.example.hippobookproject.dto.message.MessageDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
     public interface AlarmMapper {
-    List<AlarmDto> selectAlarm();
     List<AlarmDto> findByID(Long userId);
     void insertAlarm(AlarmDto alarmDto);
     void updateAlarmCheckByUserId(Long userId);
 
     Boolean hasUncheckedAlarms(Long userId);
+
+//    메시지 알림
+    List<MessageDto> selectMessageAlarm();
+
+    List<FollowDto> selectFollowAlarm();
 
     }
