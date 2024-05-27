e0f08e104e9fb08adfc4c7e463d56958209fffe2
Dashboard <kkung3535@naver.com>
2024-05-27 10:15:17 +0900

    js수정

--- src/main/resources/static/js/categorie/BookCate.js
+++ src/main/resources/static/js/categorie/BookCate.js
@@ -1,4 +1,4 @@
-{
+
 $(document).ready(function ($){
     $(".category").click(function (event){
         console.log(".category");
@@ -15,12 +15,11 @@ $(document).ready(function ($){
             1);
     })
 })
-    $(document).ready(function ($){
-        $(".editor").click(function (event){
-            console.log(".editor");
-            event.preventDefault();
-            $('html,body').animate({scrollTop:$(this.hash).offset().top},
-                1);
-        })
+$(document).ready(function ($){
+    $(".editor").click(function (event){
+        console.log(".editor");
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top},
+            1);
     })
-}
+})
