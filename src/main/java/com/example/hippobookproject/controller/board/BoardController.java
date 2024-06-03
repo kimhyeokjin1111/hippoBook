8efd5bd84698d58e50303715d4a2dbfdc227c647
kimhyeokjin1111 <r3rumasi2@gmail.com>
2024-06-03 13:44:03 +0900

    게시판 조회수 기능 수정

--- src/main/java/com/example/hippobookproject/controller/board/BoardController.java
+++ src/main/java/com/example/hippobookproject/controller/board/BoardController.java
@@ -2,12 +2,14 @@ package com.example.hippobookproject.controller.board;
 
 import com.example.hippobookproject.dto.board.PostSearchResultDto;
 import com.example.hippobookproject.service.board.BoardService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
@@ -27,8 +29,14 @@ public class BoardController {
     }
 
     @GetMapping("/post/view")
-    public String postView(Long postId, String postType, Model model){
-        log.info("postId = " + postId + ", postType = " + postType);
+    public String postView(Long postId, String postType, Model model,
+                           @SessionAttribute(value = "userId" , required = false) Long userId){
+//        log.info("postId = " + postId + ", postType = " + postType);
+        log.info("postId = " + postId + ", postType = " + postType + ", model = " + model + ", userId################ = " + userId);
+        if(userId != null){
+            boardService.registerPostView(postId, userId, postType);
+        }
+
         PostSearchResultDto postDto = boardService.selectPostById(postId, postType);
         log.info("postDto = {}", postDto);
         model.addAttribute("postDto", postDto);
