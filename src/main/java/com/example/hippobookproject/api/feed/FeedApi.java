b3a7651c535fc55ab78cdc56dcd20e68bbb9a76a
Kimsj112 <sujeong6254@gmail.com>
2024-06-04 10:32:48 +0900

    DB 최신화, POST API 파일 생성, 책 모달 창 동적쿼리 수정

---
+++ src/main/java/com/example/hippobookproject/api/feed/FeedApi.java
@@ -0,0 +1,22 @@
+package com.example.hippobookproject.api.feed;
+
+import com.example.hippobookproject.dto.feed.FollowDto;
+import com.example.hippobookproject.service.feed.FeedService;
+import com.example.hippobookproject.service.feed.PostService;
+import com.example.hippobookproject.service.feed.ReadService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class FeedApi {
+    private final PostService postService;
+
+    @PostMapping("/v1/search")
+    public void search(){
+
+    }
+}
